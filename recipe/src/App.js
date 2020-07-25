0aec8ea136d1429d10604178e6b344222faecd13
osidekyle <hawkinskyle67@gmail.com>
2020-07-25 01:23:04 -0700

    beginning app.js html

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+
+  const APP_ID = 'f077f25a';
+  const APP_KEY='660c5ad267fee3ba0159fe56ad5ae00d';
+
+  const EXAMPLE_REQUEST=`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return(
+    <div className="app">
+      <form className="search-form">
+        <input type="text" className="search-bar"></input>
+          <button className="search-button" type="submit">Search</button>
+        
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
